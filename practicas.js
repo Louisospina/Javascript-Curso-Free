1fb8e5cf6b22609bbe6aae4b15a88b7763a7df95
Luis Bastidas <117583088+Louisospina@users.noreply.github.com>
2023-04-16 15:49:34 +0200

    Doble archivo

---
+++ practicas.js
@@ -0,0 +1,26 @@
+/* Estos serían los comentarios*/
+______________________________________________________________
+/*
+
+Tipos de Datos
+
+Undefined= No existe una valor asignado
+Null= No existe
+Boolean= Verdadero y falso
+String= Cadenas de caracteres
+Symbol=
+Number= Numero
+Object= Objeto
+*/
+______________________________________________________________
+/*
+Variables
+
+
+*/
+var miVariable = "Primera variable";
+console.log(miVariable);
+______________________________________________________________
+/*
+
+*/
