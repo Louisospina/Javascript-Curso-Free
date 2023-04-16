51ea7113b9a1bd54f5bf7a3c454f406c3d106ff0
Luis Bastidas <117583088+Louisospina@users.noreply.github.com>
2023-04-16 17:47:26 +0200

    Operaciones matematicas

--- practicas.js
+++ practicas.js
@@ -1,6 +1,6 @@
 /* Estos serían los comentarios
 ______________________________________________________________
-/* Tipos de Datos
+Tipos de Datos
 
 Undefined= No existe una valor asignado
 Null= No existe
@@ -10,8 +10,35 @@ Symbol=
 Number= Numero
 Object= Objeto
 ______________________________________________________________
-/* Variables
+Variables
 
-*/
 var miVariable = "Primera variable";
 console.log(miVariable);
+______________________________________________________________
+Operadoraciones matemáticas
+
+a) Obtener el resto de una división:
+
+var resto = 5 % 3;
+console.log(resto); // El resto de 5 / 3 es 2
+
+b) Código más conciso:
+
+var suma = 10;
+suma = suma + 1; console.log(suma); //se le suma 1
+suma++;          console.log(suma); //se le suma 1 también
+suma += 100;     console.log(suma); //se le suma cualquier número
+
+var resta = 20;
+resta = resta - 1; console.log(resta); //se le resta 1
+resta--;           console.log(resta); //se le resta 1 también
+resta -= 100;      console.log(resta); //se le resta cualquier número
+
+var multiplicación = 8;
+multiplicación = multiplicación * 10; console.log(multiplicación); //se le multiplicación 10
+multiplicación *= 10;                 console.log(multiplicación); //se multiplica por 10 también
+
+var división = 8;
+división = división / 2; console.log(división); //se le división 2
+división /= 2;           console.log(división); //se divide por 2 también
+______________________________________________________________ */
